055ab1b70798dd2f2f755f0b0dae2a2e38d54b08
rachelbak <racheli.b.job@gmail.com>
2025-12-31 16:11:34 +0200

    Visual test clear

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 // const api = axios.create({
 //   baseURL:'https://escapevr-server.onrender.com',
 // });
+
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
