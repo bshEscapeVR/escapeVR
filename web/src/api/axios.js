c7bebc44a745f2f9b6c3f1cfc95be4c841125ce2
rachelbak <racheli.b.job@gmail.com>
2025-12-31 03:52:06 +0200

    maby

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 // משתמש במשתנה הסביבה, ואם אין - ברירת מחדל לשרת בענן
 const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://escapevr-server.onrender.com';
 
+console.log('=== AXIOS DEBUG ===');
+console.log('NEXT_PUBLIC_API_URL:', process.env.NEXT_PUBLIC_API_URL);
+console.log('baseURL:', baseURL);
+
 const api = axios.create({
   baseURL: baseURL,
 });
