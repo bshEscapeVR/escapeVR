8ef307a2b540104818fe5577cd606e785b58c70b
rachelbak <racheli.b.job@gmail.com>
2025-12-31 16:10:33 +0200

    Visual test clear

--- web/src/api/axios.js
+++ web/src/api/axios.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL:'https://escapevr-server.onrender.com',
-});
 // const api = axios.create({
-//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+//   baseURL:'https://escapevr-server.onrender.com',
 // });
+const api = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
 
 // Interceptor להוספת הטוקן
 api.interceptors.request.use(
