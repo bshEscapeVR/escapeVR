fe6c537bad0938e76b8215d692d017352fc52da5
rachelbak <racheli.b.job@gmail.com>
2025-12-31 03:14:56 +0200

    maby

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: 'http',
-  //       hostname: 'localhost',
-  //       port: '5000',
-  //       pathname: '/uploads/**',
-  //     },
-  //   ],
-  // },
   images: {
-  remotePatterns: [
-    {
-      protocol: 'https',
-      hostname: 'escapevr-server.onrender.com',
-      pathname: '/uploads/**',
-    },
-  ],
-},
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '5000',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+//   images: {
+//   remotePatterns: [
+//     {
+//       protocol: 'https',
+//       hostname: 'escapevr-server.onrender.com',
+//       pathname: '/uploads/**',
+//     },
+//   ],
+// },
 };
 
 export default nextConfig;
