3c2d99cf72abcb9f454a91ee56b4cfa68d5b2713
rachelbak <racheli.b.job@gmail.com>
2026-01-01 19:36:35 +0200

    2

--- web/src/services/http.service.js
+++ web/src/services/http.service.js
@@ -1,4 +1,20 @@
-import api from '../api/axios';
+import axios from 'axios';
+
+const API_URL = 'https://escapevr-server.onrender.com';
+
+console.log('🚀 Service interacting with:', API_URL);
+
+const api = axios.create({
+    baseURL: API_URL
+});
+
+api.interceptors.request.use(config => {
+    if (typeof window !== 'undefined') {
+        const token = localStorage.getItem('token');
+        if (token) config.headers['x-auth-token'] = token;
+    }
+    return config;
+});
 
 const httpService = {
   get: async (endpoint, params = {}) => {
