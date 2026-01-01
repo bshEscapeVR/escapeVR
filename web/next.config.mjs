2e28550db7fa4428c0ca51e128e1f7ebaf249b8b
rachelbak <racheli.b.job@gmail.com>
2026-01-01 22:47:45 +0200

    tryconf

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -1,5 +1,24 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'res.cloudinary.com',
+//         pathname: '/dh1lickcoh/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // 1. הוספת המיפוי הזה מכריחה את המשתנה להיכנס לתוך ה-Build
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  },
   images: {
     remotePatterns: [
       {
@@ -7,6 +26,11 @@ const nextConfig = {
         hostname: 'res.cloudinary.com',
         pathname: '/dh1lickcoh/**',
       },
+      // הוספנו גם את השרת שלך ליתר ביטחון (למקרה שיש תמונות משם)
+      {
+        protocol: 'https',
+        hostname: 'escapevr-server.onrender.com', 
+      }
     ],
   },
 };
